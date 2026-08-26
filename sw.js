e9286f986a6bdc7cca86729247713b4efdabdcd8
seitaro0902 <117574248+seitaro0902@users.noreply.github.com>
2026-08-26 15:21:08 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'sw25-db-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './data.json',
+  './manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
